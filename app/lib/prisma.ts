ffb1eb815ab43e350124c4d5bd569dc676ec5458
Anthony <anthonydoanwork@gmail.com>
2025-02-04 21:12:37 -0600

    Completed User Auth 7

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -4,10 +4,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+// Ensure Prisma Client is properly initialized before exporting
+prisma.$connect().catch((error) => {
+  console.error("Failed to connect to the database:", error);
+});
